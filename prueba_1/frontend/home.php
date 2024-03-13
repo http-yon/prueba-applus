6fbe1a38d7f219b8b36483d4ed828976760dc7ff
http-yon <yohanpedraza2006@gmail.com>
2024-03-13 17:18:00 -0500

    feat: :tada: inicio prueba applus - crud de productos
    estructura de carpetas provisional y base de datos

---
+++ prueba_1/frontend/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> prueba tecnica | APPLUS  </title>
+</head>
+<body>
+    <H1> prueba tecnica applus | crud </H1>
+</body>
+</html>
