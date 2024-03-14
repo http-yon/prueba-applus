da27068cadcc266da4130f2f5711cd946c1edd94
http-yon <yohanpedraza2006@gmail.com>
2024-03-13 19:24:20 -0500

    feat: :sparkles: clases categoria y productos
    se crearon las clases de categoria y producto con sus respectivos metodos y atributos (CRUD)

---
+++ prueba_1/backend/models/Categoria.php
@@ -0,0 +1,109 @@
+<?php 
+
+require_once(__DIR__ . "/config/pdo.php");
+
+class Categoria extends Conectar {
+    
+    private $categoriaId;
+    private $nombreCategoria;
+    private $fechaCreacionCategoria;
+    private $fechaActualizacionCategoria;
+
+    public function __construct($categoriaId = 0, $nombreCategoria = "", $fechaCreacionCategoria = "", $fechaActualizacionCategoria = "") {
+        $this->categoriaId = $categoriaId;
+        $this->nombreCategoria = $nombreCategoria;
+        $this->fechaCreacionCategoria = $fechaCreacionCategoria;
+        $this->fechaActualizacionCategoria = $fechaActualizacionCategoria;
+        parent::__construct();
+    }
+
+    //getters
+    public function getCategoriaId(){
+        return $this->categoriaId;
+    }
+
+    public function getNombreCategoria(){
+        return $this->nombreCategoria;
+    }
+
+    public function getFechaCreacionCategoria(){
+        return $this->fechaCreacionCategoria;
+    }
+
+    public function getFechaActualizacionCategoria(){
+        return $this->fechaActualizacionCategoria;
+    }
+
+    //setters
+    public function setCategoriaId($categoriaId){
+        $this->categoriaId = $categoriaId;
+    }
+
+    public function setNombreCategoria($nombreCategoria){
+        $this->nombreCategoria = $nombreCategoria;
+    }
+
+    public function setFechaCreacionCategoria($fechaCreacionCategoria){
+        $this->fechaCreacionCategoria = $fechaCreacionCategoria;
+    }
+
+    public function setFechaActualizacionCategoria($fechaActualizacionCategoria){
+        $this->fechaActualizacionCategoria = $fechaActualizacionCategoria;
+    }
+
+    //GET
+    public function obtenerCategorias(){
+        try {
+            $stm = $this->db->prepare("SELECT * FROM Category");
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    //GET ONE
+    public function obtenerUnaCategoria(){
+        try {
+            $stm = $this->db->prepare("SELECT * FROM Category WHERE id = ?");
+            $stm->execute([$this->categoriaId]);
+            return $stm->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+    
+    //POST
+    public function insertarCategorias(){
+        try {
+            $stm = $this->db->prepare("INSERT INTO Category(name, createdAt, updatedAt ) VALUES (?, ?, ?)");
+            $stm->execute([$this->nombreCategoria, $this->fechaCreacionCategoria, $this->fechaActualizacionCategoria]);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    //DELETE
+    public function borrarCategorias(){
+        try {
+            $stm = $this->db->prepare("DELETE FROM Category WHERE id = ?");
+            $stm->execute([$this->categoriaId]);
+            return true;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    //UPDATE
+    public function editarCategorias(){
+        try {
+            $stm = $this->db->prepare("UPDATE Category SET name = ?, createdAt = ?, updatedAt = ? WHERE id = ?"); // Corregido 'empleadoId' a 'id'
+            $stm->execute([$this->nombreCategoria, $this->fechaCreacionCategoria, $this->fechaActualizacionCategoria, $this->categoriaId]);
+            return true;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}   
+
+
---
+++ prueba_1/backend/models/Producto.php
@@ -0,0 +1,137 @@
+<?php 
+
+require_once(__DIR__. "/config/pdo.php");
+
+class Producto extends Conectar{
+    
+    private $productoId;
+    private $codigoProducto;
+    private $nombreProducto;
+    private $categoria_id;
+    private $precioProducto;
+    private $createdAtProducto;
+    private $updatedAtProducto;
+
+    public function __construct($productoId = 0, $codigoProducto = "", $nombreProducto = "", $categoria_id = 0, $precioProducto = 0.0, $createdAtProducto = "", $updatedAtProducto = ""){
+        $this->productoId = $productoId;
+        $this->codigoProducto = $codigoProducto;
+        $this->nombreProducto = $nombreProducto;
+        $this->categoria_id = $categoria_id;
+        $this->precioProducto = $precioProducto;
+        $this->createdAtProducto = $createdAtProducto;
+        $this->updatedAtProducto = $updatedAtProducto;
+        parent::__construct();
+    }
+
+    //getters
+    public function getProductoId(){
+        return $this->productoId;
+    }
+
+    public function getCodigoProducto(){
+        return $this->codigoProducto;
+    }
+
+    public function getNombreProducto(){
+        return $this->nombreProducto;
+    }
+
+    public function getCategoria_id(){
+        return $this->categoria_id;
+    }
+
+    public function getPrecioProducto(){
+        return $this->precioProducto;
+    }
+
+    public function getCreatedAtProducto(){
+        return $this->createdAtProducto;
+    }
+
+    public function getUpdatedAtProducto(){
+        return $this->updatedAtProducto;
+    }
+
+    //setters
+    public function setProductoId($productoId){
+        $this->productoId = $productoId;
+    }
+
+    public function setCodigoProducto($codigoProducto){
+        $this->codigoProducto = $codigoProducto;
+    }
+
+    public function setNombreProducto($nombreProducto){
+        $this->nombreProducto = $nombreProducto;
+    }
+
+    public function setCategoria_id($categoria_id){
+        $this->categoria_id = $categoria_id;
+    }
+
+    public function setPrecioProducto($precioProducto){
+        $this->precioProducto = $precioProducto;
+    }
+
+    public function setCreatedAtProducto($createdAtProducto){
+        $this->createdAtProducto = $createdAtProducto;
+    }
+
+    public function setUpdatedAtProducto($updatedAtProducto){
+        $this->updatedAtProducto = $updatedAtProducto;
+    }
+
+   //GET
+   public function obtenerProductos(){
+    try {
+        $stm = $this->db->prepare("SELECT * FROM Product");
+        $stm->execute();
+        return $stm->fetchAll();
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+    }
+
+    //GET ONE
+    public function obtenerUnProducto(){
+        try {
+            $stm = $this->db->prepare("SELECT * FROM Product WHERE id = ?");
+            $stm->execute([$this->productoId]);
+            return $stm->fetchAll();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    //POST
+    public function insertarProducto() {
+        try {
+            $stm = $this->db->prepare("INSERT INTO Product(code, name, category_id, price, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?)");
+            $stm->execute([$this->codigoProducto, $this->nombreProducto, $this->categoria_id, $this->precioProducto, $this->createdAtProducto, $this->updatedAtProducto]);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+        //DELETE
+        public function borrarProducto (){
+            try {
+                $stm = $this->db->prepare("DELETE FROM Product WHERE id = ?");
+                $stm->execute([$this->productoId]);
+                return $stm->fetchAll();
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
+        }
+
+    //UPDATE
+    public function editarProducto(){
+        try {
+            $stm = $this->db->prepare("UPDATE Product SET code = ?, name = ?, category_id = ?, price = ?, createdAt = ?, updatedAt = ? WHERE id = ?");
+            $stm->execute([$this->codigoProducto, $this->nombreProducto, $this->categoria_id, $this->precioProducto, $this->createdAtProducto, $this->updatedAtProducto, $this->productoId]);
+            return $stm->fetchAll();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
